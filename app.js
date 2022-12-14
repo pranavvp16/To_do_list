2f36127523930ecb4ed8d410d9abf6430c386cdd
Pranav Prajapati <pranavprajapati586@gmail.com>
2022-12-14 23:27:47 +0530

    form to add more task in todolist

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+let items = ["Study Chemistry" , "Complete assignment" , "Make flappyBirdAI"];
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('veiw engine','ejs');
 
-app.get("/",function(res,req){
-    res.send("Hello")
+
+app.get("/",function(req,res){
+    let today = new Date();
+    let options = {weekday: "long" , day: "numeric" , month: "long"};
+    let day = today.toLocaleDateString('en-US',options);
+    res.render("index" , {kindOfDay:day,newItems:items});
 });
+
+app.post("/",function(req,res){
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
+
 
 app.listen(3000,function(){
     console.log("App running at port 3000")
